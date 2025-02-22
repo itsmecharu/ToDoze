0b1bcf68aa42ddebef7c35b0fa2a69c0b79368e3
itsmecharu <happycharu2004@gmail.com>
2025-02-22 13:39:24 +0545

    fixing profile.php

--- profile.php
+++ profile.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['userid'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Analytics</title>
+    <title>Profile</title>
     <link rel="stylesheet" href="css/profile.css">
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     
