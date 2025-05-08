70d3af267d680ddae823f40cfa75b755cdb6284f
itsmecharu <happycharu2004@gmail.com>
2025-05-08 13:29:28 +0545

    arranged code and did css

--- profile.php
+++ profile.php
@@ -89,14 +89,9 @@ $userid = $_SESSION['userid'];
 <!-- Scripts -->
 <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script>
 
+<script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 
-  
-    <!-- ===== IONICONS ===== -->
-    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
-
-    <!-- ===== MAIN JS ===== -->
     <script src="js/dash.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
